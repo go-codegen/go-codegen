f099deeb1de03f8623b8fb5acebfb9ce9608f654
Artem <113848666+qwent71@users.noreply.github.com>
2023-08-12 02:29:05 +0300

    Update struct.go

--- internal/parse/struct.go
+++ internal/parse/struct.go
@@ -5,6 +5,7 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"log"
 	"os"
 	"regexp"
 	"strings"
@@ -24,12 +25,59 @@ type ParsedStruct struct {
 	Fields       []ParsedField
 }
 
-func parseFile(filename string) (*ast.File, error) {
-	fset := token.NewFileSet()
-	return parser.ParseFile(fset, filename, nil, parser.ParseComments)
+type StructImpl struct {
+	fset *token.FileSet
+	f    *ast.File
 }
 
-func parseTags(tag string) map[string][]string {
+func NewStruct() *StructImpl {
+	return &StructImpl{}
+}
+
+func (s *StructImpl) parseFile(filename string) (*ast.File, error) {
+	s.fset = token.NewFileSet()
+	var err error
+	s.f, err = parser.ParseFile(s.fset, filename, nil, parser.ParseComments)
+	return s.f, err
+}
+
+// parse dir
+func (s *StructImpl) parseDir(path string) ([]*ast.File, error) {
+
+	//parse last part of path
+	parts := strings.Split(path, "/")
+	lastPart := parts[len(parts)-1]
+
+	var pathWithoutLastPart string
+	for i := 0; i < len(parts)-1; i++ {
+		pathWithoutLastPart += parts[i] + "/"
+	}
+
+	//find files in path
+	fInDir, err := os.ReadDir(pathWithoutLastPart)
+	if err != nil {
+		return nil, err
+	}
+	var files []*ast.File
+	for _, file := range fInDir {
+		if strings.Contains(file.Name(), lastPart) {
+			newPath := pathWithoutLastPart + file.Name()
+			fset := token.NewFileSet()
+			pkgs, err := parser.ParseDir(fset, newPath, nil, parser.ParseComments)
+			if err != nil {
+				return nil, err
+			}
+			for _, pkg := range pkgs {
+				for _, f := range pkg.Files {
+					files = append(files, f)
+				}
+			}
+		}
+	}
+	return files, nil
+}
+
+func (s *StructImpl) parseTags(tag string) map[string][]string {
 	tags := make(map[string][]string)
 	tag = strings.Trim(tag, "`")
 	tagRegExp := regexp.MustCompile(`([\w]+):"([^"]+)"`)
@@ -39,10 +87,48 @@ func parseTags(tag string) map[string][]string {
 	return tags
 }
 
-func collectFields(fields []*ast.Field) []ParsedField {
+func (s *StructImpl) findStructByName(structs [][]ParsedStruct, name string) []ParsedStruct {
+	var result []ParsedStruct
+	for _, innerSlice := range structs {
+		for _, s := range innerSlice {
+			if s.StructName == name {
+				result = append(result, s)
+			}
+		}
+	}
+	return result
+}
+
+func (s *StructImpl) findFileByNameImport(name string) ([][]ParsedStruct, error) {
+	for _, imp := range s.f.Imports {
+		imp.Path.Value = strings.Trim(imp.Path.Value, "\"")
+		parts := strings.Split(imp.Path.Value, "/")
+		lastPart := parts[len(parts)-1]
+		if lastPart == name {
+			gopath := os.Getenv("GOPATH") // also consider "GOROOT" depending on your configuration
+			if gopath == "" {
+				log.Fatalf("No GOPATH set")
+			}
+			path := gopath + "/pkg/mod/" + imp.Path.Value
+			path = strings.Replace(path, "\\", "/", -1)
+			files, err := s.parseDir(path)
+			if err != nil {
+				return nil, err
+			}
+			var parsedStructs [][]ParsedStruct
+			for _, f := range files {
+				parsedStructs = append(parsedStructs, s.parseStructs(f.Name.Name, f))
+			}
+
+			return parsedStructs, nil
+		}
+	}
+	return nil, fmt.Errorf("no file found")
+}
+
+func (s *StructImpl) collectFields(fields []*ast.Field) []ParsedField {
 	var parsedFields []ParsedField
 	for _, field := range fields {
-		fmt.Println(field.Type)
 		switch fieldType := field.Type.(type) {
 		case *ast.StructType:
 			// If the field type is a struct, recursively collect its fields
@@ -53,7 +139,7 @@ func collectFields(fields []*ast.Field) []ParsedField {
 				pstruct := ParsedStruct{StructName: fieldName}
 
 				// Parse and collect fields from the nested struct
-				pstruct.Fields = collectFields(fieldType.Fields.List)
+				pstruct.Fields = s.collectFields(fieldType.Fields.List)
 
 				// Construct ParsedField with nested struct information
 				parsedField := ParsedField{
@@ -65,6 +151,35 @@ func collectFields(fields []*ast.Field) []ParsedField {
 				parsedFields = append(parsedFields, parsedField)
 			}
 		case *ast.SelectorExpr:
+			if fmt.Sprintf("%v", fieldType.X) == "time" {
+				parsedFields = append(parsedFields, ParsedField{
+					Name: field.Names[0].Name,
+					Type: fmt.Sprintf("%v.%s", fieldType.X, fieldType.Sel.Name),
+					NestedStruct: &ParsedStruct{
+						StructName:   fieldType.Sel.Name,
+						StructModule: fmt.Sprintf("%v", fieldType.X),
+					},
+				})
+				continue
+			}
+			structures, err := s.findFileByNameImport(fmt.Sprintf("%v", fieldType.X))
+			if err != nil {
+				return nil
+			}
+			structs := s.findStructByName(structures, fieldType.Sel.Name)
+			if len(structs) > 0 {
+				parsedFields = append(parsedFields, ParsedField{
+					Name: fmt.Sprintf("%v.%s", fieldType.X, fieldType.Sel.Name),
+					Type: "struct",
+					NestedStruct: &ParsedStruct{
+						StructName:   fieldType.Sel.Name,
+						StructModule: fmt.Sprintf("%v", fieldType.X),
+						FileName:     structs[0].FileName,
+						Fields:       structs[0].Fields,
+					},
+				})
+				continue
+			}
 			// add struct to nested structs
 			parsedFields = append(parsedFields, ParsedField{
 				Name: fmt.Sprintf("%v.%s", fieldType.X, fieldType.Sel.Name),
@@ -84,9 +199,21 @@ func collectFields(fields []*ast.Field) []ParsedField {
 			}
 		case *ast.Ident:
 			// If the field type is a struct, recursively collect its fields
-			if fieldType.Obj != nil {
-				fields := collectFields(fieldType.Obj.Decl.(*ast.TypeSpec).Type.(*ast.StructType).Fields.List)
 
+			//TODO ADD SUPPORT FOR EMBEDDED STRUCTS
+
+			if fieldType.Obj != nil {
+				decl, ok := fieldType.Obj.Decl.(*ast.TypeSpec)
+				if !ok {
+					log.Fatal("Not a *ast.TypeSpec")
+				}
+
+				astStruct, ok := decl.Type.(*ast.StructType)
+				if !ok {
+					log.Fatal("Not a *ast.StructType")
+				}
+
+				fields := s.collectFields(astStruct.Fields.List)
 				var name string
 				if len(field.Names) > 0 {
 					name = field.Names[0].Name
@@ -105,7 +232,7 @@ func collectFields(fields []*ast.Field) []ParsedField {
 			if len(field.Names) > 0 {
 				tags := map[string][]string{}
 				if field.Tag != nil {
-					tags = parseTags(field.Tag.Value)
+					tags = s.parseTags(field.Tag.Value)
 				}
 				parsedFields = append(parsedFields, ParsedField{
 					Name: field.Names[0].Name,
@@ -116,16 +243,14 @@ func collectFields(fields []*ast.Field) []ParsedField {
 			}
 
 		default:
-			fmt.Println("Unknown field type", fieldType)
-			//	print field type
-			fmt.Println(fieldType)
+			continue
 		}
 
 	}
 	return parsedFields
 }
 
-func processSpecs(decl *ast.GenDecl, filename string) []ParsedStruct {
+func (s *StructImpl) processSpecs(decl *ast.GenDecl, filename string) []ParsedStruct {
 	var structs []ParsedStruct
 	for _, spec := range decl.Specs {
 		typeSpec, ok := spec.(*ast.TypeSpec)
@@ -136,33 +261,34 @@ func processSpecs(decl *ast.GenDecl, filename string) []ParsedStruct {
 		if !ok {
 			continue
 		}
-		fields := collectFields(structType.Fields.List)
+		fields := s.collectFields(structType.Fields.List)
+
 		structs = append(structs, ParsedStruct{FileName: filename, StructName: typeSpec.Name.Name, Fields: fields})
 	}
 	return structs
 }
 
-func parseStructs(filename string, file *ast.File) []ParsedStruct {
+func (s *StructImpl) parseStructs(filename string, file *ast.File) []ParsedStruct {
 	var structs []ParsedStruct
 	for _, decl := range file.Decls {
 		genDecl, ok := decl.(*ast.GenDecl)
 		if !ok {
 			continue
 		}
-		structs = append(structs, processSpecs(genDecl, filename)...)
+		structs = append(structs, s.processSpecs(genDecl, filename)...)
 	}
 	return structs
 }
 
 func main() {
 	filename := "test/test.go"
-	file, err := parseFile(filename)
+	s := StructImpl{}
+	file, err := s.parseFile(filename)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println("parse")
-	structs := parseStructs(filename, file)
+	structs := s.parseStructs(filename, file)
 	for _, s := range structs {
 		printStruct(s)
 	}
